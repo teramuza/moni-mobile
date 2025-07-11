e80cf8a60de6aabb93ba83d63d4982b9627a55ee
Teuku Raja <tera@flip.id>
2025-07-12 03:18:55 +0700

    fix useFocusEffect

--- src/navigations/InitialRoute.hooks.ts
+++ src/navigations/InitialRoute.hooks.ts
@@ -1,24 +1,24 @@
-import { useEffect, useState } from 'react';
+import { useCallback, useState } from 'react';
 import { useAuthStore } from '@stores/AuthStore';
+import { useFocusEffect } from '@react-navigation/native';
 
-type InitialRoute =
-  | 'Dashboard'
-  | 'Loading'
-  | 'Login';
+type InitialRoute = 'Dashboard' | 'Loading' | 'Login';
 
 export function useInitialRoute() {
   const { user } = useAuthStore();
   const [route, setRoute] = useState<InitialRoute>('Loading');
 
-  useEffect(() => {
-    if (!user?.id || user?.status !== 1) {
-      setRoute('Login');
-      return;
-    }
+  useFocusEffect(
+    useCallback(() => {
+      if (!user?.id || user?.status !== 1) {
+        setRoute('Login');
+        return;
+      }
 
-    setRoute('Dashboard');
-    return;
-  }, [user?.id]);
+      setRoute('Dashboard');
+      return;
+    }, [user?.id]),
+  );
 
   return route;
 }
